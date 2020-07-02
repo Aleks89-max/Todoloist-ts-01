28bf8f4cf6a1a7508be81122da50ba9f34efafae
Aleks89-max <greisvandir89@gmail.com>
2020-07-02 16:11:59 +0300

    useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import {TodoList} from "./TodoList";
 export type TasksType = {
@@ -7,26 +7,28 @@ export type TasksType = {
     isDone: boolean
 }
 
-
-let tasks1: Array<TasksType> = [
-    { id:1, title: 'HTML', isDone: false  },
-    { id:2, title: 'JS', isDone: false  },
-    { id:3, title: 'Css', isDone: false  },
-    { id:4, title: 'Rest Api', isDone: false  },
-    { id:5, title: 'Graph Ql', isDone: false  }
-]
-function removeTask (id: number) {
-   tasks1 = tasks1.filter((t) => t.id !== id
-    )
-    console.log(tasks1)
-
-}
-
 function App() {
+    let [tasks, setTasks] = useState <Array<TasksType>>(
+        [
+            { id:1, title: 'HTML', isDone: false  },
+            { id:2, title: 'JS', isDone: false  },
+            { id:3, title: 'Css', isDone: false  },
+            { id:4, title: 'Rest Api', isDone: false  },
+            { id:5, title: 'Graph Ql', isDone: false  }
+        ]
+    )
+
+
+    function removeTask (id: number) {
+        let  filterTask = tasks.filter((t) => t.id !== id)
+        setTasks(filterTask)
+    }
+
+
     return (
         <div className="App">
             <TodoList title ={'What to learn'}
-                      task={tasks1}
+                      task={tasks}
                       removeTask={removeTask}/>
 
 
